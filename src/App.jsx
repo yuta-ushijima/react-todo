36f745e9debf52a3ea7d3798648803d360d5e5f2
Yuta Ushijima <register@yuta-u.com>
2021-12-05 17:02:17 +0900

    追加ボタンを押したら元のテキストは初期化して削除

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ export const App = () => {
   const onClickAdd = () => {
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
+    setTodoText("");
   };
   return (
     <>
